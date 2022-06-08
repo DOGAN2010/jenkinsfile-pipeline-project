cbab31c9ff8fe6709e26a5513f583a1bfa93f36c
DOGAN2010 <doganbilisim2010@gmail.com>
2022-06-08 20:13:44 +0300

    hello java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java new");
+        System.out.println("Hello from Java. HELLO DOGAN");
     }
 }
